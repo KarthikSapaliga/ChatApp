4f6c5565cfd32e363c51c3e57f2753e0c952b7bd
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-06-15 23:30:36 +0530

    added global state for contact list and defined addMessage to store message

--- client/src/store/slices/chatSlice.js
+++ client/src/store/slices/chatSlice.js
@@ -2,12 +2,39 @@ export const createChatSlice = (set, get) => ({
     selectedChatType: undefined,
     selectedChatData: undefined,
     selectedChatMessages: [],
-    setSelectedChatTypes: (selectedChatType) => set({ selectedChatType }),
+    DMContacts: [],
+    setSelectedChatType: (selectedChatType) => set({ selectedChatType }),
     setSelectedChatData: (selectedChatData) => set({ selectedChatData }),
-    setSelectedChatMessages: (selectedChatMessages) =>set({ selectedChatMessages }),
-    closeChat: () => set({ 
-        selectedChatType: undefined, 
-        selectedChatData: undefined,
-        selectedChatMessages: [], 
-    }),
+    setSelectedChatMessages: (selectedChatMessages) =>
+        set({ selectedChatMessages }),
+    closeChat: () =>
+        set({
+            selectedChatType: undefined,
+            selectedChatData: undefined,
+            selectedChatMessages: [],
+        }),
+    addMessage: (message) => {
+        console.log("inside the add message:");
+        console.log(message);
+        const selectedChatMessages = get().selectedChatMessages;
+        const selectedChatType = get().selectedChatType;
+
+        set({
+            selectedChatMessages: [
+                ...selectedChatMessages,
+                {
+                    ...message,
+                    recipient:
+                        selectedChatType === "channel"
+                            ? message.recipient
+                            : message.recipient._id,
+                    sender:
+                        selectedChatType === "channel"
+                            ? message.sender
+                            : message.sender._id,
+                },
+            ],
+        });
+    },
+    setDMContacts: (DMContacts) => set({ DMContacts }),
 });
