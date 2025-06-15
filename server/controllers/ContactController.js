2988dec17fd8d4b86eb4c65dbcddd7604907be11
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-06-15 23:22:41 +0530

    added getContacts to get the list of contacts

--- server/controllers/ContactController.js
+++ server/controllers/ContactController.js
@@ -1,5 +1,7 @@
 import createError from "../utils/createError.js";
 import User from "../models/UserModel.js";
+import Message from "../models/MessageModel.js";
+import mongoose from "mongoose";
 
 export const searchContacts = async (req, res, next) => {
     try {
@@ -20,6 +22,81 @@ export const searchContacts = async (req, res, next) => {
                 },
             ],
         }).select("-password");
+
+        console.log("contacts");
+        console.log(contacts);
+
+        res.status(200).json(contacts);
+    } catch (error) {
+        console.log(error.message);
+        return next(createError(500, "Internal server error"));
+    }
+};
+
+export const getContacts = async (req, res, next) => {
+    console.log(">> get contacts list");
+
+    try {
+        let { userId } = req;
+        userId = new mongoose.Types.ObjectId(userId);
+
+        const contacts = await Message.aggregate([
+            {
+                $match: {
+                    $or: [{ sender: userId }, { recipient: userId }],
+                },
+            },
+            {
+                $sort: { timestamp: -1 },
+            },
+            {
+                $group: {
+                    _id: {
+                        $cond: {
+                            if: { $eq: ["$sender", userId] },
+                            then: "$recipient",
+                            else: "$sender",
+                        },
+                    },
+                    lastMessageTime: { $first: "$timestamp" },
+                    lastMessageText: {
+                        $first: {
+                            $cond: {
+                                if: { $eq: ["$messageType", "text"] },
+                                then: "$content",
+                                else: "[File]",
+                            },
+                        },
+                    },
+                },
+            },
+            {
+                $lookup: {
+                    from: "Users",
+                    localField: "_id",
+                    foreignField: "_id",
+                    as: "contactInfo",
+                },
+            },
+            {
+                $unwind: "$contactInfo",
+            },
+            {
+                $project: {
+                    _id: 1,
+                    lastMessageTime: 1,
+                    lastMessageText: 1,
+                    email: "$contactInfo.email",
+                    name: "$contactInfo.name",
+                    image: "$contactInfo.image",
+                },
+            },
+            {
+                $sort: {
+                    lastMessageTime: -1,
+                },
+            },
+        ]);
 
         console.log("contacts");
         console.log(contacts);
