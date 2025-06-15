c98592351971cb8216e52fb11020b02e7bebe26d
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-06-15 23:24:32 +0530

    added the get-contacts route

--- server/routes/ContactRouter.js
+++ server/routes/ContactRouter.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { verifyToken } from "../middleware/AuthMiddleware.js";
-import { searchContacts } from "../controllers/ContactController.js";
+import {
+    searchContacts,
+    getContacts,
+} from "../controllers/ContactController.js";
 
 const ContactRouter = express.Router();
 
 ContactRouter.post("/search", verifyToken, searchContacts);
+ContactRouter.get("/get-contacts", verifyToken, getContacts);
 
 export default ContactRouter;
