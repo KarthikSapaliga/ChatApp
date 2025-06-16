324d7faca901c41c339555029a63eeae53794432
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-06-16 12:14:33 +0530

    added logic to sort the chats and channels by latest

--- client/src/store/slices/chatSlice.js
+++ client/src/store/slices/chatSlice.js
@@ -3,19 +3,23 @@ export const createChatSlice = (set, get) => ({
     selectedChatData: undefined,
     selectedChatMessages: [],
     DMContacts: [],
+    channels: [],
+
     setSelectedChatType: (selectedChatType) => set({ selectedChatType }),
     setSelectedChatData: (selectedChatData) => set({ selectedChatData }),
     setSelectedChatMessages: (selectedChatMessages) =>
         set({ selectedChatMessages }),
+    setChannels: (channels) => set({ channels }),
+    setDMContacts: (DMContacts) => set({ DMContacts }),
+
     closeChat: () =>
         set({
             selectedChatType: undefined,
             selectedChatData: undefined,
             selectedChatMessages: [],
         }),
+
     addMessage: (message) => {
-        console.log("inside the add message:");
-        console.log(message);
         const selectedChatMessages = get().selectedChatMessages;
         const selectedChatType = get().selectedChatType;
 
@@ -36,5 +40,43 @@ export const createChatSlice = (set, get) => ({
             ],
         });
     },
-    setDMContacts: (DMContacts) => set({ DMContacts }),
+
+    addChannel: (channel) => {
+        const channels = get().channels;
+        set({ channels: [...channels, channel] });
+    },
+
+    addChannelInChannelList: (message) => {
+        const channels = get().channels;
+        const data = channels.find(
+            (channel) => channel._id === message.channelId
+        );
+        const index = channels.findIndex(
+            (channel) => channel._id === message.channelId
+        );
+        if (index !== -1 && index !== undefined) {
+            channels.splice(index, 1);
+            channels.unshift(data);
+        }
+    },
+
+    addContactsInDMContacts: (message) => {
+        const userId = get().userInfo.id;
+        const DMContacts = get().DMContacts;
+        const fromId =
+            message.sender._id === userId
+                ? message.recipient._id
+                : message.sender._id;
+        const fromData =
+            message.sender._id === userId ? message.recipient : message.sender;
+        const data = DMContacts.find((contact) => contact._id === fromId);
+        const index = DMContacts.findIndex((contact) => contact._id === fromId);
+        if (index !== -1 && index != undefined) {
+            DMContacts.splice(index, 1);
+            DMContacts.unshift(data);
+        } else {
+            DMContacts.unshift(fromData);
+        }
+        set({ DMContacts });
+    },
 });
