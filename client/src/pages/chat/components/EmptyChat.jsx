86b5477c8800bab48db70c4c6408a277f7b94d9a
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-06-16 20:53:06 +0530

    added the empty chat componet

---
+++ client/src/pages/chat/components/EmptyChat.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function EmptyChat() {
+    return (
+        <div className="hidden sm:flex justify-center items-center bg-light3 dark:bg-dark2 w-full">
+       
+        </div>
+    );
+}
+
+export default EmptyChat;
