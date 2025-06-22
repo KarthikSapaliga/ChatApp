276d7357b2a41cd1959a55b2ee62019f6acd1c64
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-06-22 10:12:17 +0530

    feat(auth): add route for uploading profile image using ImageKit

--- server/routes/AuthRouter.js
+++ server/routes/AuthRouter.js
@@ -16,21 +16,7 @@ import { verifyToken } from "../middleware/AuthMiddleware.js";
 
 const AuthRouter = express.Router();
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "./uploads");
-    },
-    filename: (req, file, cb) => {
-        const uniqueSuffix = Date.now() + Math.round(Math.random() + 1e9);
-        const newFileName =
-            file.fieldname +
-            "-" +
-            uniqueSuffix +
-            path.extname(file.originalname);
-
-        cb(null, newFileName);
-    },
-});
+const storage = multer.memoryStorage();
 
 const uploads = multer({ storage });
 
