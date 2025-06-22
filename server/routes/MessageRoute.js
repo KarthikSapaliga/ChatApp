f2689ce852c7087725201ddc385fb5e0c90df9e4
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-06-22 10:56:55 +0530

    feat(message): add route for uploading file using ImageKit

--- server/routes/MessageRoute.js
+++ server/routes/MessageRoute.js
@@ -2,23 +2,8 @@ import express from "express";
 import { verifyToken } from "../middleware/AuthMiddleware.js";
 import { getMessages, uploadFile } from "../controllers/MessageController.js";
 import multer from "multer";
-import path from "path";
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "./uploads");
-    },
-    filename: (req, file, cb) => {
-        const uniqueSuffix = Date.now() + Math.round(Math.random() + 1e9);
-        const newFileName =
-            file.fieldname +
-            uniqueSuffix +
-            "-" +
-            path.basename(file.originalname);
-
-        cb(null, newFileName);
-    },
-});
+const storage = multer.memoryStorage();
 const uploads = multer({ storage });
 
 const MessageRouter = express.Router();
